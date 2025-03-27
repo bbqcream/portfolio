b5b48817de59382b36fc93e5f03f960c290244b1
bbqcream <imground11@gmail.com>
2025-03-27 12:33:54 +0900

    sujeong

--- src/components/projectBox.tsx
+++ src/components/projectBox.tsx
@@ -4,7 +4,7 @@ import { ProjectBoxProps } from "./projectBoxProps";
 const ProjectBox = ({ img, title, detail, link }: ProjectBoxProps) => {
     return (
         <a href={link} target="_blank">
-            <div className="bg-white text-black p-5 flex sm:flex-col sm:gap-3 gap-5 items-center border rounded-2xl">
+            <div className="box">
                 <img src={img} className="sm:w-70 w-30" />
                 <div>
                     <div className="font-bold sm:text-4xl text-3xl">
