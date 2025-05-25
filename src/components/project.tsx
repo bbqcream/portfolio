5ec14ab9eb31c49ede381032bb7adf0aa2b91997
김성한 <imground11@gmail.com>
2025-05-25 22:43:41 +0900

    fix :: project box

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -3,13 +3,13 @@ import ProjectBox from "./projectBox";
 
 const Project = () => {
   return (
-    <div className="min-h-[100vh] min-w-[100vw] bg-black relative text-white border-b-white">
-      <div className="absolute top-[5%] left-[10%]">
+    <div className="min-h-screen min-w-[100vw] bg-black relative text-white border-b-white">
+      <div className="absolute top-[5%] left-[10%] right-[10%]">
         <div className="text-4xl md:text-6xl font-bold pt-15">
           MAIN PROJECTS
         </div>
         <div className="bg-white w-15 md:w-20 h-1 my-6" />
-        <div className=" flex flex-col sm:flex-row sm:gap-3 gap-3">
+        <div className="flex flex-wrap lg:flex-nowrap gap-4 overflow-x-visible lg:overflow-x-auto whitespace-normal lg:whitespace-nowrap pb-4">
           <ProjectBox
             title="Legacy"
             detail="게임과 함께하는 우리 문화와 역사"
@@ -33,6 +33,12 @@ const Project = () => {
             detail="IT 대회 정보를 보여주는 프로그램입니다."
             link="https://github.com/T2AM-COOK/Galendar-Web"
             img="Gal.svg"
+          />
+          <ProjectBox
+            title="VacPlanner"
+            detail="방학 플래너를 짜주는 프로그램입니다."
+            link="https://github.com/bbqcream/VacPlanner"
+            img="vacplanner.svg"
           />
         </div>
       </div>
--- src/components/projectBox.tsx
+++ src/components/projectBox.tsx
@@ -3,12 +3,16 @@ import { ProjectBoxProps } from "./projectBoxProps";
 
 const ProjectBox = ({ img, title, detail, link }: ProjectBoxProps) => {
   return (
-    <a href={link} target="_blank">
-      <div className="box">
-        <img src={img} className="sm:w-70 w-30" />
+    <a href={link} target="_blank" className="w-full">
+      <div className="box sm:h-110 sm:w-80 h-60 w-full">
+        <img src={img} className="sm:w-70 sm:h-40 w-30 h-40" />
         <div>
-          <div className="font-bold sm:text-4xl text-3xl">{title}</div>
-          <div className="font-medium">{detail}</div>
+          <div className="font-bold sm:text-4xl text-3xl whitespace-normal break-words">
+            {title}
+          </div>
+          <div className="font-medium whitespace-normal break-words">
+            {detail}
+          </div>
         </div>
       </div>
     </a>
